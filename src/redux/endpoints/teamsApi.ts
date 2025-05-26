e5daacc2ac7682faa4f1fbeae9af48b941e51599
fiamanillah <fiamanillahb2@gmail.com>
2025-05-27 03:48:52 +0600

    Team table added team update route added with the fix in select by search

--- src/redux/endpoints/teamsApi.ts
+++ src/redux/endpoints/teamsApi.ts
@@ -1,41 +1,50 @@
-import { baseApi } from "../api";
+import { baseApi } from '../api';
 
 export const teamsApi = baseApi.injectEndpoints({
-  endpoints: (build) => ({
-    teamCreate: build.mutation({
-      query: data => ({
-        url: '/teams',
-        method: 'POST',
-        credentials: 'include',
-        body: data
-      }),
-      invalidatesTags: ['team']
-    }),
+    endpoints: build => ({
+        teamCreate: build.mutation({
+            query: data => ({
+                url: '/teams',
+                method: 'POST',
+                credentials: 'include',
+                body: data,
+            }),
+            invalidatesTags: ['team'],
+        }),
 
-    allTeams: build.query({
-      query: params => ({
-        url: '/teams',
-        method: 'GET',
-        credentials: 'include',
-        params
-      }),
-      providesTags: ['team']
-    }),
+        allTeams: build.query({
+            query: params => ({
+                url: '/teams',
+                method: 'GET',
+                credentials: 'include',
+                params,
+            }),
+            providesTags: ['team'],
+        }),
 
-    singleTeam: build.query({
-      query: teamName => ({
-        url: `/teams/${teamName}`,
-        method: 'GET',
-        credentials: 'include',
-      }),
-      providesTags: ['team']
+        singleTeam: build.query({
+            query: teamName => ({
+                url: `/teams/${teamName}`,
+                method: 'GET',
+                credentials: 'include',
+            }),
+            providesTags: ['team'],
+        }),
+        teamUpdate: build.mutation({
+            query: ({ slug, data }) => ({
+                url: `/teams/${slug}`,
+                method: 'PUT',
+                credentials: 'include',
+                body: data,
+            }),
+            invalidatesTags: ['team'],
+        }),
     }),
-
-  }),
 });
 
 export const {
-  useTeamCreateMutation,
-  useAllTeamsQuery,
-  useSingleTeamQuery
+    useTeamCreateMutation,
+    useAllTeamsQuery,
+    useSingleTeamQuery,
+    useTeamUpdateMutation,
 } = teamsApi;
