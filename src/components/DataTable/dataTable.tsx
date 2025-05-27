d3aa3b0d250f1529096783db49c6e7c76ef1d994
aHasnat1997 <ahasnat557@gmail.com>
2025-05-28 00:57:07 +0600

    FIX: code.

--- src/components/DataTable/dataTable.tsx
+++ src/components/DataTable/dataTable.tsx
@@ -11,7 +11,7 @@ import {
     getFilteredRowModel,
     // type VisibilityState,
     type Table as TableType,
-} from '@tanstack/react-table';
+} from "@tanstack/react-table"
 
 import {
     Table,
@@ -20,34 +20,36 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from '@/components/ui/table';
-import { Button } from '../ui/button';
-import { Skeleton } from '../ui/skeleton';
-import React, { useState, forwardRef, useImperativeHandle, type ForwardedRef } from 'react';
+} from "@/components/ui/table"
+import { Button } from "../ui/button"
 import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-} from '../ui/pagination';
+    Skeleton
+} from "../ui/skeleton"
+import React, {
+    useState,
+    forwardRef,
+    useImperativeHandle,
+    type ForwardedRef,
+} from "react"
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink } from "../ui/pagination"
+
 
 export interface DataTableHandle<TData> {
     // table: ReturnType<typeof useReactTable<TData>>
-    table: TableType<TData>;
+    table: TableType<TData>
 }
 
 export interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[];
-    data: TData[];
-    isLoading?: boolean;
-    filterColumn?: string;
-    page: number;
-    limit: number;
-    total: number;
-    onPageChange: (page: number) => void;
-    onLimitChange: (limit: number) => void;
-    actions?: (row: TData) => React.ReactNode;
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+    isLoading?: boolean
+    filterColumn?: string
+    page: number
+    limit: number
+    total: number
+    onPageChange: (page: number) => void
+    onLimitChange: (limit: number) => void
+    actions?: (row: TData) => React.ReactNode
     // columnFilters: ColumnFiltersState
     // setColumnFilters: React.Dispatch<React.SetStateAction<ColumnFiltersState>>
     // columnVisibility: VisibilityState
@@ -64,71 +66,13 @@ function DataTableInner<TData, TValue>(
         total,
         onPageChange,
         actions,
-    }: // columnFilters,
-    // setColumnFilters,
-    // columnVisibility,
-    // setColumnVisibility,
-    DataTableProps<TData, TValue>,
+        // columnFilters,
+        // setColumnFilters,
+        // columnVisibility,
+        // setColumnVisibility,
+    }: DataTableProps<TData, TValue>,
     ref: ForwardedRef<DataTableHandle<TData>>
 ) {
-  const [sorting, setSorting] = useState<SortingState>([]);
-  // const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  // const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      sorting,
-      // columnFilters,
-      // columnVisibility,
-      pagination: {
-        pageIndex: page - 1,
-        pageSize: limit,
-      },
-    },
-    onSortingChange: setSorting,
-    // onColumnFiltersChange: setColumnFilters,
-    // onColumnVisibilityChange: setColumnVisibility,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    manualPagination: true,
-    pageCount: Math.ceil(total / limit),
-  })
-
-  useImperativeHandle(ref, () => ({ table }), [table])
-
-  const renderSkeleton = () =>
-    [...Array(limit)].map((_, rowIndex) => (
-      <TableRow key={`skeleton-${rowIndex}`}>
-        {columns.map((_, colIndex) => (
-          <TableCell key={`skeleton-cell-${colIndex}`}>
-            <Skeleton className="h-6 w-full" />
-          </TableCell>
-        ))}
-        {actions && (
-          <TableCell className="text-right flex items-center justify-end">
-            <Skeleton className="h-6 w-16" />
-          </TableCell>
-        )}
-      </TableRow>
-    ))
-
-  return (
-    <div className="h-[84vh] rounded-md border flex flex-col">
-      <div className="border-b">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id} className="text-muted-foreground">
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
     const [sorting, setSorting] = useState<SortingState>([]);
     // const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
     // const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
@@ -154,9 +98,9 @@ function DataTableInner<TData, TValue>(
         getPaginationRowModel: getPaginationRowModel(),
         manualPagination: true,
         pageCount: Math.ceil(total / limit),
-    });
+    })
 
-    useImperativeHandle(ref, () => ({ table }), [table]);
+    useImperativeHandle(ref, () => ({ table }), [table])
 
     const renderSkeleton = () =>
         [...Array(limit)].map((_, rowIndex) => (
@@ -172,23 +116,20 @@ function DataTableInner<TData, TValue>(
                     </TableCell>
                 )}
             </TableRow>
-        ));
+        ))
 
     return (
         <div className="h-[84vh] rounded-md border flex flex-col">
             <div className="border-b">
                 <Table>
                     <TableHeader>
-                        {table.getHeaderGroups().map(headerGroup => (
+                        {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map(header => (
+                                {headerGroup.headers.map((header) => (
                                     <TableHead key={header.id} className="text-muted-foreground">
                                         {header.isPlaceholder
                                             ? null
-                                            : flexRender(
-                                                  header.column.columnDef.header,
-                                                  header.getContext()
-                                              )}
+                                            : flexRender(header.column.columnDef.header, header.getContext())}
                                     </TableHead>
                                 ))}
                                 {actions && <TableHead className="text-right">Actions</TableHead>}
@@ -196,39 +137,30 @@ function DataTableInner<TData, TValue>(
                         ))}
                     </TableHeader>
                     <TableBody>
-                        {isLoading ? (
-                            renderSkeleton()
-                        ) : table.getRowModel().rows.length > 0 ? (
-                            table.getRowModel().rows.map(row => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && 'selected'}
-                                >
-                                    {row.getVisibleCells().map(cell => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
+                        {isLoading
+                            ? renderSkeleton()
+                            : table.getRowModel().rows.length > 0
+                                ? table.getRowModel().rows.map((row) => (
+                                    <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            </TableCell>
+                                        ))}
+                                        {actions && (
+                                            <TableCell className="text-right flex items-center justify-end">
+                                                {actions(row.original)}
+                                            </TableCell>
+                                        )}
+                                    </TableRow>
+                                ))
+                                : (
+                                    <TableRow>
+                                        <TableCell colSpan={columns.length + (actions ? 1 : 0)} className="h-24 text-center">
+                                            No results.
                                         </TableCell>
-                                    ))}
-                                    {actions && (
-                                        <TableCell className="text-right flex items-center justify-end">
-                                            {actions(row.original)}
-                                        </TableCell>
-                                    )}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell
-                                    colSpan={columns.length + (actions ? 1 : 0)}
-                                    className="h-24 text-center"
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
+                                    </TableRow>
+                                )}
                     </TableBody>
                 </Table>
             </div>
@@ -259,17 +191,15 @@ function DataTableInner<TData, TValue>(
                                         Math.max(0, page - 3),
                                         Math.min(Math.ceil(total / limit), page + 2)
                                     )
-                                    .map(pageNumber => (
+                                    .map((pageNumber) => (
                                         <PaginationItem key={pageNumber}>
                                             <PaginationLink
                                                 isActive={pageNumber === page}
-                                                onClick={e => {
-                                                    e.preventDefault();
-                                                    onPageChange(pageNumber);
+                                                onClick={(e) => {
+                                                    e.preventDefault()
+                                                    onPageChange(pageNumber)
                                                 }}
-                                                className={`border-primary ${
-                                                    pageNumber === page ? 'text-primary' : ''
-                                                }`}
+                                                className={`border-primary ${pageNumber === page ? 'text-primary' : ''}`}
                                             >
                                                 {pageNumber}
                                             </PaginationLink>
@@ -296,12 +226,16 @@ function DataTableInner<TData, TValue>(
                             </PaginationContent>
                         </Pagination>
                     </div>
+
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export const DataTable = forwardRef(DataTableInner) as <TData, TValue = unknown>(
+export const DataTable = forwardRef(DataTableInner) as <
+    TData,
+    TValue = unknown,
+>(
     props: DataTableProps<TData, TValue> & { ref?: React.Ref<DataTableHandle<TData>> }
-) => ReturnType<typeof DataTableInner>;
+) => ReturnType<typeof DataTableInner>
